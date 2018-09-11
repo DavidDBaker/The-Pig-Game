328aa7b033431a123f1887b662a5d31b0f3d676d
David Baker <daviiddamiianbaker@gmail.com>
2018-09-11 16:15:49 -0400

    starting to structure js content

---
+++ resources/js/pigGame.js
@@ -0,0 +1,27 @@
+var 
+scores, 
+roundScore, 
+activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function btn() {
+    // Random Number
+    var dice = Math.floor(Math.random() * 6 ) + 1;
+
+    var diceDOM =  document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'resources/images/dice-' + dice + '.png';
+});
+
+
+
